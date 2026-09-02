caea51be97c0a5111ea7192d7d80e64394711067
lisahamzah745-eng <lisahamzah745@gmail.com>
2026-09-02 20:23:40 +0800

    Day001.java

--- Day001.java
+++ Day001.java
@@ -1,1 +1,7 @@
-//komentar
+public class Day1 {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Ini hari pertama 100 Days of Coding");
+        System.out.println("Nama saya: Lisa Hamzah");
+    }
+}
