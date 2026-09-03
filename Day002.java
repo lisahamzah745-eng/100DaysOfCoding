ebd522f4bb2d8f4259dfc8b5b9ee8c876b085a59
lisahamzah745-eng <lisahamzah745@gmail.com>
2026-09-03 22:32:12 +0800

    Day002.java

--- Day002.java
+++ Day002.java
@@ -1,9 +1,24 @@
-public class day2 {
-    public static void main(String[] args) {
-        System.out.println("Nama   :Lisa Hamzah");
-        System.out.println("Jurusan:Informatika");
-        System.out.println("TTL    :Majene,9 Desember 2007");
-        System.out.printf("Hobi    :Menggambar & Baca Buku\n"); 
-        System.out.print("Umur     :19 tahun"); 
-    }
+ public class day2 {
+    public static void main(String[] args){
+        // println() digunakan untuk menampilkan teks 
+        // dan otomatis pindah ke baris baru
+        System.out.println("Nama    : Lisa Hamzah");
+
+        // println() digunakan untuk menampilkan teks
+        System.out.println("Jurusan : Informatika");
+
+        // println() digunakan untuk menampilkan teks
+        System.out.println("TTL     : Majene, 9 Desember 2007");
+
+        // println() digunakan untuk menampilkan teks
+        System.out.println("Alamat  : Majene");
+
+        // printf() digunakan untuk menampilkan teks dengan format tertentu
+        // \n digunakan untuk pindah ke baris baru
+        System.out.printf("Hobi    : Menggambar & Baca Buku\n"); 
+        
+        // print() digunakan untuk menampilkan teks
+        // tetapi TIDAK otomatis pindah ke baris berikutnya
+        System.out.print("Umur    : 19 tahun"); 
+   }
 }
