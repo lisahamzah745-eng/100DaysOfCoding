f1523a6e10d9e5ec37a9d81525066578bfca38da
lisahamzah745-eng <lisahamzah745@gmail.com>
2026-09-04 15:37:28 +0800

    Day003.java

---
+++ Day003.java
@@ -0,0 +1,29 @@
+public class Day003 {
+    public static void main(String[] args) {
+        
+        // \n fungsinya untuk menambah baris baru
+        System.out.println("Hello\nWorld");
+
+        // \t fungsinya untuk membuat jarak mendatar atau spasi
+        System.out.println("Saya\tLisa");
+
+        // \\ fungsinya menampilkan satu karakter backslash \
+        System.out.println("\\Folder\\File.java");
+
+        // \" fungsinya untuk menampilkan tanda petik dua "
+        System.out.println("Dia bilang \"Halo\"");
+
+        // \' fungsinya menampilkan tanda petik tunggal '
+        System.out.println("Saya bilang \'Hai\'");
+
+        // \r fungsinya untuk mengembalikan kursor ke awal baris yang sama dan menimpanya 
+        System.out.println("123456\rABCDE");
+
+        // \b fungsinya untuk menghapus satu karakter di sebelah kiri
+        System.out.println("Coding\b");
+
+        // \f fungsinya untuk ganti halaman atau pindah ke halaman baru
+        System.out.println("Halaman1\fHalaman2");
+
+
+}
