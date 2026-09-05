62786417e724bc3d3a597d25e2e8da4b415756b5
lisahamzah745-eng <lisahamzah745@gmail.com>
2026-09-05 14:29:35 +0800

    Day004.java

--- Day004.java
+++ Day004.java
@@ -11,16 +11,36 @@ public class Day004 {
         System.out.println("=======BYTE=======");
         System.out.println("Umur Lisa: " + umurLisa + " tahun");
         System.out.println("Suhu: " + suhu + "°C");
-        System.out.println();
-         /*
-          * SHORT 
-          * Ukuran: 2 byte = 16 bit
-          * Range: -32768 sampai 32767
-          * Fungsi: untuk menyimpan bilangan bulat sedang >127 dan <32767
-          */
-         short tahun = 2026;
-         System.out.println("=======SHORT=======");
-         System.out.println("Mahasiswa Angkatan " + tahun");
-         System.out.println();
+        /*
+         * SHORT 
+         * Ukuran: 2 byte = 16 bit
+         * Range: -32768 sampai 32767
+         * Fungsi: untuk menyimpan bilangan bulat sedang >127 dan <32767
+         */
+        short tahun = 2026;
+        System.out.println("=======SHORT=======");
+        System.out.println("Mahasiswa Angkatan " + tahun);
+        /*
+         * INT 
+         * Ukuran: 4 byte = 32 bit
+         * Range: -2.147.483.648 sampai 2.147.483.647
+         * Fungsi: Untuk tipe data bilangan bulat default. Paling sering dipakai
+         *         untuk umur, jumlah, skor, looping
+         */
+        int jumlahMahasiswa = 2686;
+        System.out.println("=======INT=======");
+        System.out.println("Jumlah Mahasiswa Baru:" +jumlahMahasiswa);
+        /*
+         * LONG
+         * Ukuran: 8 byte = 64 bit 
+         * Range: -9.223.372.036.854.775.808 sampai 9.223.372.036.854.775.807
+         * Fungsi: Untuk menyimpan angka yang sangat besar
+         * Penulisannya Wajib ada huruf L di belakang
+         * biasanya dipakai untuk jumlah penduduk, timestamp, uang dalam rupiah
+         */
+        long pendudukSulawesiBarat = 1466741L;
+        System.out.println("=======LONG=======");
+        System.out.println("Data Jumlah Penduduk Sulawesi Barat");
+        System.out.println("Jumlah penduduk 2024 :" + PendudukSulawesiBarat);
     }
 }
