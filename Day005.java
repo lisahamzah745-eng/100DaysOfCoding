0e23bebf3194221d293d8e1094994ebbf8b8b994
lisahamzah745-eng <lisahamzah745@gmail.com>
2026-09-06 12:09:34 +0800

    Day005.java

--- Day005.java
+++ Day005.java
@@ -1,10 +1,11 @@
 public class Day005 {
      public static void main(String[] args) {
      //Double yaitu tipe data bilangan desimal presisi ganda
-     //akurat sampai 15 digit di belakang koma
+     //Akurat sampai 15 digit di belakang koma
      double makanan = 10.000000000000001;
      //Float yaitu tipe data bilangan desimal presisi ganda
-     //akurat sampai 6 digit dibelakang koma
+     //Akurat sampai 6 digit dibelakang koma 
+     //Wajib ditambahkan f diakhir biar terbaca sebagai float di java
      float minuman = 10.000001f;
      
      System.out.println("Rating Makanan\t:" + makanan);
