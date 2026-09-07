360c15256d768665bb935665cb9da34eef1769a8
lisahamzah745-eng <lisahamzah745@gmail.com>
2026-09-07 20:00:58 +0800

    Day006.java

---
+++ Day006.java
@@ -0,0 +1,16 @@
+public class Day006 {
+    public static void main(String[] args) {
+        // char adalah tipe data yang digunakan untuk menyimpan satu karakter 
+        // ditulis menggunakan kutip satu ' '
+        char akreditasi = 'A';
+        // boolean adalah tipe data yang dgunakan untuk menyimpan dua nilai kemungkinan yaitu true dan false
+        boolean status = true;
+       
+        System.out.println("=======================");
+        System.out.println("Penilaian");
+        System.out.println("Akreditas\t: " + akreditasi);
+        System.out.println("Status Aktif\t: " + status);
+        System.out.println("=======================");
+
+    }
+}
