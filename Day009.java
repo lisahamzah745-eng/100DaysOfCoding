97ad1d1393c5e6578f13ea0f43fc5b1349521fa6
lisahamzah745-eng <lisahamzah745@gmail.com>
2026-09-10 17:42:36 +0800

    Day009.java

---
+++ Day009.java
@@ -0,0 +1,8 @@
+public class Day009 {
+    public static void main(String[] args) {
+        // Konstanta (final variabel), yaitu variabel yang nilainya tidak dapat diubah lagi setelah pertama kali diinisialisasi
+        final int jumlahSKS = 20;
+
+        System.out.println("Jumlah SKS yang bisa diambil: " + jumlahSKS);
+    }
+}
