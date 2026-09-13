c58113bc2e3e019acddceb72ac289a4399c0cffc
lisahamzah745-eng <lisahamzah745@gmail.com>
2026-09-13 20:06:47 +0800

    Day012.java

---
+++ Day012.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Day012 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        System.out.print("Nama: ");
+        String nama = input.nextLine();
+        System.out.print("Umur: ");
+        int umur = input.nextInt();
+        System.out.print("Tinggi: ");
+        double tinggi = input.nextDouble();
+        System.out.print("NIM: ");
+        int nim = input.nextInt();
+        System.out.print("Kelas: ");
+        char kelas = input.next().charAt(0);
+
+        System.out.println("----BIODATA MAHASISWA----");
+        System.out.println("Nama\t: " + nama);
+        System.out.println("Umur\t: " + umur + " tahun");
+        System.out.println("Tinggi\t: " + tinggi + " cm");
+        System.out.println("NIM\t: " + nim);
+        System.out.println("Kelas\t: " + kelas);
+    
+     }
+}
