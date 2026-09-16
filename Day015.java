d6834fbf4371b4b2558491e50cd6115f49b59067
lisahamzah745-eng <lisahamzah745@gmail.com>
2026-09-16 09:48:03 +0800

    Day015.java

---
+++ Day015.java
@@ -0,0 +1,13 @@
+public class Day015 {
+    public static void main(String[] args) {
+        int a = 25;
+        int b = 5;
+        System.out.println("Diketahui");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("Hasil perkalian dan pembagiannya");
+        System.out.println("a × b = " + (a*b));
+        System.out.println("a ÷ b = " + (a/b));
+    }
+    
+}
