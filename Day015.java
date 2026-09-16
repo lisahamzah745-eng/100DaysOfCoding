fd7e115443f442d690e4c2941493c961203e8896
lisahamzah745-eng <lisahamzah745@gmail.com>
2026-09-16 16:06:55 +0800

    Day015.java

--- Day015.java
+++ Day015.java
@@ -8,6 +8,12 @@ public class Day015 {
         System.out.println("Hasil perkalian dan pembagiannya");
         System.out.println("a × b = " + (a*b));
         System.out.println("a ÷ b = " + (a/b));
-    }
-    
+        
+        // Catatan!!
+        // pembagi ≠ 0 ,karena jika pembagi = 0 maka pembagian tidak dapat dilakukan
+        // contohnya, misalkan saya buat variabel tambahan = 0
+        // int c = 0;
+        // System.out.println(b/c);   yaitu menampilkan hasil (5/0) 
+        // hasilnya itu akan error dan program akan langsung berhenti
+    }  
 }
