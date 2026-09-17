af518885be1509b24abfddb886fb0b56cf2ffff2
lisahamzah745-eng <lisahamzah745@gmail.com>
2026-09-17 21:13:22 +0800

    Day016.java

---
+++ Day016.java
@@ -0,0 +1,13 @@
+public class Day016 {
+    
+    public static void main(String[] args) {
+        int a = 12;
+        int b = 4;
+        int c = 5;
+        int hasil = a%c;
+        System.out.println("Hasil modulus atau sisa pembagian");
+        System.out.println("12 % 4 = " +(a%b));
+        System.out.println("12 % 5 = " + hasil);
+    }
+    
+}
