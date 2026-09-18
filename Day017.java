22663f42623d043474c20e496bccc7c873bce21b
lisahamzah745-eng <lisahamzah745@gmail.com>
2026-09-18 20:44:54 +0800

    Day017.java

---
+++ Day017.java
@@ -0,0 +1,18 @@
+public class Day017 {
+    public static void main (String[] args) {
+        System.out.println("Operator penugasan");
+        int nilai = 2026;
+        System.out.println("Nilai Awal\t= " +nilai);
+        nilai += 10;
+        System.out.println("Nilai + 10\t= " +nilai);
+        nilai -= 36;
+        System.out.println("Hasil - 36\t= " + nilai);
+        nilai *= 5;
+        System.out.println("Hasil × 5\t= " + nilai);
+        nilai /= 2;
+        System.out.println("Hasil ÷ 2\t= " + nilai);
+        nilai %= 17;
+        System.out.println("Hasil % 17\t= " + nilai);
+        System.out.println("Nilai Akhir\t= " + nilai);
+    }
+}
