1619c9ff661e298ed21aba255923b2213f02c3f7
lisahamzah745-eng <lisahamzah745@gmail.com>
2026-09-19 20:06:07 +0800

    Day018.java

---
+++ Day018.java
@@ -0,0 +1,13 @@
+public class Day018 {
+    public static void main(String[] args){
+        byte nilaiByte = 18;
+        short nilaiShort = nilaiByte;
+        int nilaiInt = nilaiShort;
+        long nilaiLong = nilaiInt;
+        System.out.println("Mengubah tipe data kecil ke tipe data besar");
+        System.out.println("Tipe data byte\t\t: " +nilaiByte);
+        System.out.println("Tipe data byte ke short\t: " +nilaiShort );
+        System.out.println("Tipe data short ke int\t: " +nilaiInt);
+        System.out.println("Tipe data Int ke long\t: " +nilaiLong);
+    }
+}
