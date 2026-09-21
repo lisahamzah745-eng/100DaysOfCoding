1298f357cd0c906d2dc842d4b7c156213df32bc5
lisahamzah745-eng <lisahamzah745@gmail.com>
2026-09-21 19:10:45 +0800

    Day020.java

---
+++ Day020.java
@@ -0,0 +1,15 @@
+public class Day020 {
+    public static void main (String[] args){
+        int umur = 20;
+        String Umur = "" +umur;
+        System.out.println(Umur);
+        
+        double suhu = 36.0;
+        String Suhu = String.valueOf(suhu);
+        System.out.println(Suhu + " C");
+        
+        long saldoDana = 1000;
+        String SaldoDana = Long.toString(saldoDana);
+        System.out.println(SaldoDana);    
+    }
+}
